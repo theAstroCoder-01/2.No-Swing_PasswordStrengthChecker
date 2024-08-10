d948e266aa1253e0dce5677fd47b11c51b518d6c
The_Astro_Coder <153275094+theAstroCoder-01@users.noreply.github.com>
2024-08-10 11:19:52 -0600

    Add files via upload

---
+++ PasswordStrengthChecker.java
@@ -0,0 +1,58 @@
+package PasswordStrengthChecker;
+
+import java.util.Scanner;
+
+public class PasswordStrengthChecker {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Input a password: ");
+		String password = input.nextLine();
+		input.close();
+		
+		String strengthFeedback = checkPasswordStrength(password);
+		System.out.println(strengthFeedback);
+		
+	}
+	
+	public static String checkPasswordStrength(String password) {
+		
+		// Define criteria
+		int lengthCriteria = 8;
+		boolean hasUpperCase = false;
+		boolean hasLowerCase = false;
+		boolean hasDigit = false;
+		boolean hasSpecial = false;
+		String speialCharacters = "!@#$%^&*()_-+=<>?";
+		
+		//Check length
+		if(password.length() >= lengthCriteria) {
+			
+			//Check other criteria
+			for(char c : password.toCharArray()) {
+				if(Character.isUpperCase(c)) {
+					hasUpperCase = true;
+				} else if (Character.isLowerCase(c)) {
+					hasLowerCase = true;
+				} else if (Character.isDigit(c)) {
+					hasDigit = true;
+				} else if (speialCharacters.contains(String.valueOf(c))) {
+					hasSpecial = true;
+				}
+			}
+			
+			//Analize password strength
+			if(hasUpperCase & hasLowerCase & hasDigit & hasSpecial) {
+				return "It is strong password!";
+			} else {
+				return "The password is weak. Include more characters in the password";
+			}
+			
+		} else {
+			return "Password length must be at least " + lengthCriteria + " characters!";
+			
+		}
+	}
+}
